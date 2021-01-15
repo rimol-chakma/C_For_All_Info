e791857d710d27bb043c5248acdf1b6c0fb53f20
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2021-01-15 10:03:53 +0600

    Update FS.c

--- Finding a couple of number sequence/FS.c
+++ Finding a couple of number sequence/FS.c
@@ -23,3 +23,49 @@ int main()
      }
     return 0;
 }
+//Alternative method
+/*print these sequence
+
+1,2,3
+
+1,3,2
+
+2,1,3
+
+2,3,1
+
+3,1,2
+
+3,2,1
+
+*/
+
+#include<stdio.h>
+
+int main()
+
+{
+
+  int a,b,c;
+
+   for(a =1;a<=3;a++){
+
+     for(b=1;b<=3;b++){
+
+          for(c=1;c<=3;c++){
+
+               if(b!=a &&c!=a &&c!=b){
+
+                printf("%d,%d,%d\n",a,b,c);
+
+                  }
+
+               }
+
+            }
+
+        }
+
+    return 0;
+
+}
