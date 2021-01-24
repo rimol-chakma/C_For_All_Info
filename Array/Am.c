29063536e0ca412b229408e3c7f4b05630a9e241
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2021-01-24 14:05:24 +0600

    Update Am.c

--- Array/Am.c
+++ Array/Am.c
@@ -1,3 +1,31 @@
+//Array simple example
+#include<stdio.h>
+
+ 
+
+int main(){
+
+    int ara[4] = {5,10,15,20};
+
+    ara[0] = 100;
+
+    ara[2] = 200;
+
+    ara[3] = 300;
+
+    printf("%d,%d,,%d,%d\n",ara[0],ara[1],ara[2],ara[3]);
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
 //Array's easy method to placing alternatively of index 
 
 #include<stdio.h>
