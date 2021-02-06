0a9a264751a9ec5b6fa85a342ad2201520d97694
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2021-02-06 20:25:36 +0600

    Create Lnt.c

---
+++ Test_prime_number/Long_number_test/Lnt.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+
+int is_prime_number(int n){
+
+    int i;
+
+    if(n<2){
+
+        return 0;//boolean false
+
+    }
+
+    if(n==2){
+
+        return 1;
+
+    }
+
+    if(n%2==0){
+
+        return 0;
+
+    }
+
+    for(i=3;i<=n/2;i=i+2){
+
+        if(n%i==0){
+
+            return 0;//boolean false
+
+        }
+
+    }
+
+    return 1;//boolean true
+
+}
+
+int main(){
+
+    int n;
+
+    while(1)//it will execute till the break statement & it means boolean true.
+
+    {
+
+        printf("Please enter a number: ");
+
+        scanf("%d",&n);
+
+       if(1==is_prime_number(n)){
+
+       printf("%d is a prime number.\n",n);   
+
+        }
+
+        else{
+
+            printf("%d is not the prime number\n",n);
+
+        }
+
+      if(n==0){
+
+            break;
+
+        }
+
+    }
+
+    return 0;//return 0 & return 1 both are same in main function.
+
+}
