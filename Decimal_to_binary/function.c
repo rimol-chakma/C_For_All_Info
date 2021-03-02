76e85348fc3091cb80cdb3d255f90eef3d441d5b
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2021-03-02 09:05:07 +0600

    Create function.c

---
+++ Decimal_to_binary/function.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void decimal_to_binary(int n){
+    int c,k;
+    for(c=32;c>=0;c--){
+        k = n>>c ;
+    if(k&1)
+        printf("1");
+      else
+        printf("0");
+    }
+    printf("\n");
+}
+int main(){
+    int m;
+    printf("Enter any decinal nunber : ");
+    scanf("%d",&m);
+    printf("%d in binary number system is:\n", m);
+    decimal_to_binary(m);
+    return 0;
+}
